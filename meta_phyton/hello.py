ec9338ba60fab0b999e07ad0c32c710797ef7e68
farhanwaseer <59307895+farhanwaseer@users.noreply.github.com>
2023-12-10 01:23:33 +0000

    python first commit with meta

---
+++ meta_phyton/hello.py
@@ -0,0 +1,119 @@
+print("hello world farhan")
+
+x = str(3)
+y = int(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+# using casting 
+
+w = str(20)
+e = int(34)
+r = float(21)
+print(w)
+print(e)
+print(r)
+
+q = 34
+print(q)
+
+q = float(34)
+print(q)
+
+# using type(x,y,z) function
+
+print(type(x))
+print(type(e))
+print(type(q))
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+
+
+q, w, e = "Farhan", "Ahmed", "Waseer" 
+
+print(q)
+print(w)
+print(e)
+
+print(q,w,e)
+
+a = s = d = "Red Light"
+
+print(a)
+print(s)
+print(d)
+
+# unpacking 
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+print(10,y)
+
+print("//////////////////")
+
+Name = ["Farhan", "Ahmed", "Waseer"]
+q, w, e = Name
+
+print(q)
+print(w)
+print(e)
+
+print(q,w,e)
+print(q + w + e)
+print(Name)
+print("//////////////////")
+
+code =[ """ x, y, z = "Orange", "Banana", "Cherry"
+
+print(x)
+print(y)
+print(z)
+print(10,y)
+
+print("//////////////////")
+
+Name = ["Farhan", "Ahmed", "Waseer"]
+q, w, e = Name
+
+print(q)
+print(w)
+print(e)
+
+print(q,w,e)
+print(q + w + e)
+print(Name)
+""" ]
+print(code)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
+
+
+y = "every thing will be right"
+
+def myMood():
+
+    x = "Hope" 
+    print(x,"let is " + y)
+myMood()    
+    
