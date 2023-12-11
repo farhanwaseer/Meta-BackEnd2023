4dcd8a0469197d60819ed7910f028ee88a9eb1ce
farhanwaseer <59307895+farhanwaseer@users.noreply.github.com>
2023-12-11 19:10:13 +0000

    jhk

--- meta_python/functions.py
+++ meta_python/functions.py
@@ -1,5 +1,4 @@
 #### Calculate bill tex
-
 def calculate_tex(bill, tex_rate):
     return round((bill * tex_rate) / 100, 2)
 
@@ -10,12 +9,13 @@ texRate = int(input("Enter Tex rate %: "))
 
 print("Calculate tex 2 : ",calculate_tex(mybill,texRate))
 
-### Marks to percentage 
+
+## Marks to percentage 
 
 def calculate_marks(total, obtain):
-    return (obtain * 100 / total)
+    return 
 
 total_marks = int(input("Total Marks : "))
 obtain_marks = int(input("Obtain Marks : "))
 
-print("Your Marks percentage is ", calculate_marks(total_marks,obtain_marks))
+print("Your Marks percentage is ", calculate_marks(total_marks,obtain_marks), "%") 
