706d986b0e3b2ba1d496e6e7588158109cdda2ea
farhanwaseer <59307895+farhanwaseer@users.noreply.github.com>
2023-12-11 18:49:08 +0000

    funtions

--- meta_python/funstions.py
+++ meta_python/funstions.py
@@ -1,2 +1,11 @@
 #### Calculate bill tex
 
+def calculate_tex(bill, tex_rate):
+    return (bill * tex_rate) / 100
+
+print("Calculate tex: ",calculate_tex(345,14) )
+
+mybill = int(input("Enter Bill: "))
+texRate = int(input("Enter Tex rate %: "))
+
+print("Calculate tex 2 : ",calculate_tex(mybill,texRate))
