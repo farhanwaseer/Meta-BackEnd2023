34b1387c7228f3c6221666fe7f7f52915c154843
farhanwaseer <59307895+farhanwaseer@users.noreply.github.com>
2023-12-19 18:49:55 +0000

    ..

--- meta_python/exceptions.py
+++ meta_python/exceptions.py
@@ -1,5 +1,13 @@
 def divide_by(a , b):
     return a / b
 
-print(divide_by(40,4))
-print(divide_by(40,0))   
+
+try:
+    ans = print(divide_by(40,0))
+
+except:
+    print("Some thing went wrong! ")
+
+  
+
+   
