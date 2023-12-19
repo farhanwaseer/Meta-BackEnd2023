866643625c66b5c6a031ada8ecbe40ae2148b60d
farhanwaseer <59307895+farhanwaseer@users.noreply.github.com>
2023-12-19 19:15:57 +0000

    ..

--- meta_python/exceptions.py
+++ meta_python/exceptions.py
@@ -1,8 +1,17 @@
 def divide_by(a , b):
     return a / b
 
+# Step 1
 
+'''
+try:
+    ans = print(divide_by(40,0))
 
+except Exception as e:
+    print("Some thing went wrong! ",e)
+'''
+
+# Step 2
 
 try:
     ans = divide_by(54,0)
@@ -12,16 +21,11 @@ except ZeroDivisionError as e:
     print(e, ",We cannot divide by zero")
 except Exception as e:
     print(e, ",Some thing went wrong!")
-    print(e.__class__)
+    
+  #  print(e.__class__)
 
 
-'''
-try:
-    ans = print(divide_by(40,0))
 
-except Exception as e:
-    print("Some thing went wrong! ",e)
-'''
   
 
    
