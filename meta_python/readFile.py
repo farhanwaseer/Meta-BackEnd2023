c72ac5386fa3de099fec8a039f402035904db729
farhanwaseer <59307895+farhanwaseer@users.noreply.github.com>
2023-12-20 21:34:30 +0000

    ..

--- meta_python/readFile.py
+++ meta_python/readFile.py
@@ -1,3 +1,23 @@
+# Way 3 
+
+with open('test.txt', 'r') as file:
+    data = file.readlines()
+    print(data)
+
+
+# Way 2
+
+'''
+
+file = open('test.txt', 'r')
+
+data = file.readlines()
+
+print(data)
+
+file.close()
+
+'''
 # Way 1
 
 '''
@@ -9,10 +29,3 @@ print(data)
 
 '''
 
-# Way 2
-
-file = open('test.txt', 'r')
-
-data = file.readlines()
-
-print(data)
