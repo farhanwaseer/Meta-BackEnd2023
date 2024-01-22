bd34ba804a8a162b291f0b6bb1a689e0b358ea9f
farhanwaseer <59307895+farhanwaseer@users.noreply.github.com>
2024-01-22 16:18:43 +0000

    sa

--- _Python/meta_practice/prac.py
+++ _Python/meta_practice/prac.py
@@ -6,3 +6,9 @@ with open('text.txt', 'r') as file:
       data = file.readlines()
       print(data)
  
+
+ with open('text.txt', 'r') as file:
+      data = file.readlines()
+      print(data)
+ 
+ 
