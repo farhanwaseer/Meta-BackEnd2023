7c95ab78a4c6c2b557c58b4d43c08b5b3650b3f5
farhanwaseer <59307895+farhanwaseer@users.noreply.github.com>
2024-01-26 12:58:01 +0000

    dada

--- _Python/meta_practice/prac.py
+++ _Python/meta_practice/prac.py
@@ -26,7 +26,7 @@ except FileNotFoundError as e:
     print(e, "Error")
 
 with open('newtest.txt',  'a') as file:
-    file.writelines(["\n This is new line for file created 2","\n This is new line 3"])
+    file.writelines(["\n This is new line for file created 2","\n This is new line 3, \nthis is line 3 for view"])
 
 with open('newtest.txt', 'r') as file:
     data = file.readlines()
