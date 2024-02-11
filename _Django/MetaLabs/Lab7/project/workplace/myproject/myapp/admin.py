e1f2acfe8d7cf808c48ffa56c1c1e795b3ffd235
farhanwaseer <59307895+farhanwaseer@users.noreply.github.com>
2024-02-11 23:18:54 +0000

    ahdw

---
+++ _Django/MetaLabs/Lab7/project/workplace/myproject/myapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+# Import the models from models.py
+
+# Register your models here.
---
+++ _Django/MetaLabs/Lab7/project/workplace/myproject/myapp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+# Create model DrinksCategory 
+
+# Create model Drinks
